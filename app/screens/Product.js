a06bf295c907a9cfd8e384b74f8f8c3bb1ca3d99
Juan Diego <102696740+juandiego252@users.noreply.github.com>
2024-01-22 08:45:42 -0500

    Soy el estudiante 1

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,23 +1,37 @@
 import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
-    return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+    return (
+      <View style={styles.container}>
+        <Text>Bienvenido a mi tienda, Soy Juan</Text>
+        <Text style={styles.productText1}>Audifonos</Text>
+        <Text style={styles.productText2}>Escritorio</Text>
+        <Text style={styles.productText3}>Monitores</Text>
         <Button
-            title='HOME'
-            onPress={()=>{
-                navigation.navigate('HomeNav');
-            }}
+          title="HOME"
+          onPress={() => {
+            navigation.navigate("HomeNav");
+          }}
         />
-    </View>
+      </View>
+    );
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  productText1: {
+    color: "blue",
+  },
+  productText2: {
+    color: "red",
+  },
+  productText3: {
+    color: "green",
+  },
+});
